cb6b3407b91871964919dc6fd163f30eeb88ffc8
Pavon Dunbar <pavondunbar@gmail.com>
2026-03-19 11:52:17 -0700

    Added 2 new tables to Postgres schema

---
+++ target/debug/deps/withdrawal-03311a82b8e1daa6.d
@@ -0,0 +1,10 @@
+/Users/pavondunbar/CRYPTO-CUSTODY-RUST/target/debug/deps/withdrawal-03311a82b8e1daa6.d: src/main.rs src/errors.rs src/models.rs src/outbox.rs src/ports.rs src/service.rs
+
+/Users/pavondunbar/CRYPTO-CUSTODY-RUST/target/debug/deps/libwithdrawal-03311a82b8e1daa6.rmeta: src/main.rs src/errors.rs src/models.rs src/outbox.rs src/ports.rs src/service.rs
+
+src/main.rs:
+src/errors.rs:
+src/models.rs:
+src/outbox.rs:
+src/ports.rs:
+src/service.rs:
---
+++ target/debug/deps/withdrawal-ccbc328643c81d27.d
@@ -0,0 +1,10 @@
+/Users/pavondunbar/CRYPTO-CUSTODY-RUST/target/debug/deps/withdrawal-ccbc328643c81d27.d: src/main.rs src/errors.rs src/models.rs src/outbox.rs src/ports.rs src/service.rs
+
+/Users/pavondunbar/CRYPTO-CUSTODY-RUST/target/debug/deps/libwithdrawal-ccbc328643c81d27.rmeta: src/main.rs src/errors.rs src/models.rs src/outbox.rs src/ports.rs src/service.rs
+
+src/main.rs:
+src/errors.rs:
+src/models.rs:
+src/outbox.rs:
+src/ports.rs:
+src/service.rs:
